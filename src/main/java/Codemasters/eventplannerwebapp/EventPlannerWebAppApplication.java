666d2f929411e0b742cc771b704068b364ea99fe
BraydenT <braydenhtanji@gmail.com>
2023-09-14 16:00:38 -0700

    init java files with Spring setup

---
+++ src/main/java/Codemasters/eventplannerwebapp/EventPlannerWebAppApplication.java
@@ -0,0 +1,13 @@
+package Codemasters.eventplannerwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlannerWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlannerWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Codemasters/eventplannerwebapp/EventPlannerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Codemasters.eventplannerwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlannerWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
