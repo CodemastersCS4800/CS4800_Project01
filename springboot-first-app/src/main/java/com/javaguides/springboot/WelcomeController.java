ebd0a91050839becdf8a9b77c7e9a45112a05fbe
BraydenT <braydenhtanji@gmail.com>
2023-09-20 13:44:09 -0700

    added google guava library and made a function to test

--- springboot-first-app/src/main/java/com/javaguides/springboot/WelcomeController.java
+++ springboot-first-app/src/main/java/com/javaguides/springboot/WelcomeController.java
@@ -2,11 +2,32 @@ package com.javaguides.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import java.util.List;
+import java.util.Map;
+
 
 @RestController
 public class WelcomeController {
     @GetMapping ("/welcome")
     public String welcome(){
+	printItems();
         return "Welcome to Team Code Masters!!!";
+    }
+
+    public void printItems(){
+    	 Map items = ImmutableMap.of("coin", 3, "glass", 4, "pencil", 1);
+
+        items.entrySet()
+                .stream()
+                .forEach(System.out::println);
+        
+        List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi", 
+                "mandarin", "date", "quince");
+        
+        for (String fruit: fruits) {
+            System.out.println(fruit);
+        }
     }
 }
