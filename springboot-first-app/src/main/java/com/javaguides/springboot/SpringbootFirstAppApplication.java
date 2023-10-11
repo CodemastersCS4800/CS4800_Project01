57a355dd3acdc6e7b666ce0c11b5ed93afc6e00a
charlesTrue <charles.trouilliere@gmail.com>
2023-10-11 08:53:56 -0700

    Testing Web App code commit

--- springboot-first-app/src/main/java/com/javaguides/springboot/SpringbootFirstAppApplication.java
+++ springboot-first-app/src/main/java/com/javaguides/springboot/SpringbootFirstAppApplication.java
@@ -1,24 +1,12 @@
 package com.javaguides.springboot;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
 
 @SpringBootApplication
 public class SpringbootFirstAppApplication {
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)  {
 		SpringApplication.run(SpringbootFirstAppApplication.class, args);
-		Document doc = Jsoup.connect("http://www.javatpoint.com").get();
-		Elements links = doc.select("a[href]");
-		for (Element link : links) {
-			System.out.println("\nlink : " + link.attr("href"));
-			System.out.println("text : " + link.text());
-		}
-
 	}
 }
